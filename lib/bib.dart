09ff98da4b57e5883a45d0d4067aab4a968b0857
ShadowBlade9000 <bladeshadow9000@gmail.com>
2024-12-19 10:01:42 -0800

    Update bib.dart

--- lib/bib.dart
+++ lib/bib.dart
@@ -6,7 +6,21 @@ class Bib extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("bib here"),
+      body: Text("Bibliography
+
+Honadmin. "Egyptian Social Classes and Society: How Exactly Did It Work?" History, 16 Feb. 2023, www.historyonthenet.com/egyptian-social-classes.
+This source let me learn more about the social class of Egyptians. It clearly explains what Nobles did and how they help. In all this information help me understand Egyptian Nobles.\n
+"West Legon, Ghana: A Guide to This Vibrant Neighborhood – Kingsgate LifestyleKingsgate Lifestyle." Kingsgate Entertainment - Accommodation & Venue - Thohoyandou, mykingsgate.co.za/info/nobles-in-ancient-egypt-1679.
+This source helped me learn that Nobles collected taxes. The Nobles also had many servants. The Nobles would have servants that would make them extravagant food.\n
+"Egyptian Social Structure [ushistory.org]." US History, www.ushistory.org/civ/3b.asp.
+This source helped me learn that the Egyptians thought that the pharaohs were equivalent to gods. And that Egyptians\n\n  
+My Secondary Sources:\n 
+ Book: Mind of Egypt
+
+https://historylink101.com/n/egypt_1
+
+https://irp-cdn.multiscreensite.com/04cd93d4/files/uploaded/Y3%20History%20Lesson%204%20Social%20Groups.pdf
+"),
     );
   }
 }
